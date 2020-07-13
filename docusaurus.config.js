56ff2745bb7df89c2d8bbc429e583f5c1eac3032
MDzkM <mdzkm17@gmail.com>
2020-07-13 13:44:43 +0700

    Fix activeBasePath

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,7 +16,6 @@ module.exports = {
       links: [
         {
           to: '/',
-          activeBasePath: 'docs',
           label: 'Tutorials and Archives',
           position: 'right',
         },
